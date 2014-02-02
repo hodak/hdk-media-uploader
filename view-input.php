a8cfa19840e8bc5657d1c3293b52a128dad5ef3d
hodak <kaczor555@gmail.com>
2014-02-02 23:11:12 +0100

    Small changes in view

--- view-input.php
+++ view-input.php
@@ -1,8 +1,8 @@
 <div class="hdk-media-uploader">
 
-	<input type="text" value="<?php echo $args['attachment_id']; ?>" name="<?php echo $args['name']; ?>" />
+	<input type="hidden" value="<?php echo $args['attachment_id']; ?>" name="<?php echo $args['name']; ?>" />
 
-	<?php // todo merge these into one ?>
+	<?php # I thought about merging these into one but I believe readability is improved this way ?>
 	<?php if( $attachment ) : ?>
 
 		<div class="hdk-media-uploader-photo-wrap">
@@ -13,12 +13,14 @@
 	<?php else : ?>
 
 		<div class="hdk-media-uploader-photo-wrap hidden">
-			<a href="#" class="hdk-media-uploader-del-photo">x</a>
 			<img src="" alt="" class="hdk-media-uploader-photo" />
+			<a href="#" class="hdk-media-uploader-del-photo">unattach</a>
 		</div><!-- .hdk-media-uploader-photo-wrap -->
 
 	<?php endif; ?>
 
-	<button class="hdk-media-uploader-button <?php echo $args['button_class']; ?> <?php echo $attachment ? 'hidden' : ''; ?>"><?php echo $args['button_text']; ?></button> 
+	<button class="hdk-media-uploader-button <?php echo $args['button_class']; ?> <?php echo $attachment ? 'hidden' : ''; ?>">
+		<?php echo $args['button_text']; ?>
+	</button> 
 
 </div><!-- .hdk-media-uploader -->
